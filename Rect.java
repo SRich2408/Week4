6044a743c4d08e75be2fbd5b2d88f71c20602472
Sebastian Richardson <sebastian.nicolasrg24@outlook.com>
2024-02-26 14:38:20 -0500

    Create Rect.java

---
+++ Rect.java
@@ -0,0 +1,16 @@
+/***************
+ * Project: Example 2
+ * Programmer: Sebastian Richardson
+ * Date: 2/26/2024
+ ****************/
+
+public class Rect{ 
+    public static void main (String[] args){
+        System.out.println("***************");
+        System.out.println("*             *");
+        System.out.println("*             *");
+        System.out.println("***************");
+
+    }
+    
+}
