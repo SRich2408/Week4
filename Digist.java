7e932026891a1253d220228eb565236f52e1eb31
Sebastian Richardson <sebastian.nicolasrg24@outlook.com>
2024-02-28 14:22:11 -0500

    Update Digist.java

--- Digist.java
+++ Digist.java
@@ -1,9 +1,9 @@
 public class Digist {
     public static void main (String[] args){
-        int num = 258;
+        int num = 898;
         int ones = num % 10;
-        int tens = num % 100 - ones;
-        int hundreds = num % 1000 - tens - ones;
+        int tens = (num % 100 - ones)/ 10;
+        int hundreds = (num % 1000 - tens - ones)/ 100;
         System.out.println("The hundreds place is " + hundreds); 
         System.out.println("The tens place is " + tens);
         System.out.println("The ones place is " + ones);
