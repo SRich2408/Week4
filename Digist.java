5187ecb8a649f9a1887ac48210dd390709e8d11a
Sebastian Richardson <sebastian.nicolasrg24@outlook.com>
2024-02-28 14:57:42 -0500

    Update Digist.java

--- Digist.java
+++ Digist.java
@@ -1,12 +1,20 @@
+import java.util.Scanner;
 public class Digist {
     public static void main (String[] args){
-        int num = 898;
-        int ones = num % 10;
-        int tens = (num % 100 - ones)/ 10;
-        int hundreds = (num % 1000 - tens - ones)/ 100;
-        System.out.println("The hundreds place is " + hundreds); 
-        System.out.println("The tens place is " + tens);
-        System.out.println("The ones place is " + ones);
+        int number;
+
+        Scanner input = new Scanner(System.in);
+        
+        System.out.println("Enter a 3 digit number:");
+        number = input.nextInt();
+
+        
+        int ones = number % 10;
+        int tens = (number % 100 - ones)/ 10;
+        int hundreds = (number % 1000 - tens - ones)/ 100;
+        System.out.println("The hundreds place is " + number + " is " + hundreds); 
+        System.out.println("The tens place is " + number + " is " + tens);
+        System.out.println("The ones place is " + number + " is 389" + ones);
 
     }
 }
